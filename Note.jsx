34b9fc7087c00d5b02f47dae6378e4147bf4973c
Aiswarya4444 <85479062+Aiswarya4444@users.noreply.github.com>
2021-06-07 12:57:55 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note(){
+   return(
+     <div className="note">
+         <h1>Javascript and react.js</h1>
+         <p>Shape AI Sessios where really useful and effective.I Was so happy to be a part of it.it was so useful that sessions can be rewatched too.</p>
+     </div>
+    );
+  }
+
+
+export default Note;
